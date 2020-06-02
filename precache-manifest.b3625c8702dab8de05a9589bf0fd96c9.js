cb301ab255f06631c709e2c6d446616b64f18948
pinomartinUTN <110933@tecnicatura.frc.utn.edu.ar>
2020-06-02 11:36:22 -0300

    Commit

---
+++ precache-manifest.b3625c8702dab8de05a9589bf0fd96c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26dfda732be395bef66cec0b1209c0b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aff8fb2e5c6c207ed28",
+    "url": "/static/css/main.673e2f22.chunk.css"
+  },
+  {
+    "revision": "ac86e5223be9403f1f09",
+    "url": "/static/js/2.8cf603fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8cf603fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aff8fb2e5c6c207ed28",
+    "url": "/static/js/main.291815cb.chunk.js"
+  },
+  {
+    "revision": "aff873d54bc2cf281a92",
+    "url": "/static/js/runtime-main.c7e29863.js"
+  }
+]);
